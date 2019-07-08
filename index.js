ad523aab4318ee4264dd8a134496fe211dd89418
MMangus <mjmangus16@yahoo.com>
2019-07-08 15:14:17 -0400

    server is running on port 5000 and contains the GET all users request

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+const db = require("./data/db");
+
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({ error: "There was an error" }));
+});
+
+server.listen(5000, () => console.log("Server running on port 5000"));
